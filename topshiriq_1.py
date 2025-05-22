cd25ee9e2b46e5cd256b142474d00233074dd5cc
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-22 14:11:56 +0500

    dsjhv

--- topshiriq_1.py
+++ topshiriq_1.py
@@ -4,3 +4,5 @@ import requests
 https://example.com sahifasiga GET so'rov yuboring
 va javob holatini (status_code) ekranga chiqaring.
 """
+r=requests.get("https://example.com")
+print(r.status_code)
