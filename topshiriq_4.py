e087633652c5b8f3f0f057b80c73fcee75143425
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-22 14:28:56 +0500

    fkbs

--- topshiriq_4.py
+++ topshiriq_4.py
@@ -4,3 +4,7 @@ import requests
 3-topshiriqdagi JSON javobdan "title" maydonini
 ajratib olib konsolga chiqaring.
 """
+param={'title': 'delectus aut autem'}
+url=('https://jsonplaceholder.typicode.com/todos/1')
+r=requests.get(url,params=param)
+print(r.url)
