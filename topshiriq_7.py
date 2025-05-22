c91130d159efb2bce2c0f59284c81d7f018650bf
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-22 17:27:06 +0500

    sfkb

--- topshiriq_7.py
+++ topshiriq_7.py
@@ -4,3 +4,8 @@ import requests
 https://jsonplaceholder.typicode.com/comments sahifasiga
 `postId=1` parametrini yuboring va natijani ko'rsating.
 """
+postId=78
+r=requests.get(f"https://jsonplaceholder.typicode.com/comments?result={postId}")
+data=r.json()
+for i in data:
+  print(i)
