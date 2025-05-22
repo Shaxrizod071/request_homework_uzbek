93e65c43a50d018b7d772c81a1ef9254d394fc82
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-22 17:12:19 +0500

    kdfb

--- topshiriq_5.py
+++ topshiriq_5.py
@@ -5,3 +5,6 @@ https://jsonplaceholder.typicode.com/users manzilidan
 foydalanuvchilar ro'yxatini oling va
 ularning ismlarini chiqarib bering.
 """
+r=requests.get('https://jsonplaceholder.typicode.com/users')
+data=r.json()
+print(data)
--- topshiriq_6.py
+++ topshiriq_6.py
@@ -4,3 +4,7 @@ import requests
 5-topshiriqdagi ro'yxat nechta foydalanuvchidan
 iborat ekanligini hisoblang va konsolga chiqaring.
 """
+r=requests.get('https://jsonplaceholder.typicode.com/users')
+data=r.json()
+for i in data:
+   print(len(i))
