a883b987d011d1e5cba6c49836b9c7700e0b435f
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-22 14:17:18 +0500

    sdfkb

--- topshiriq_3.py
+++ topshiriq_3.py
@@ -4,3 +4,6 @@ import requests
 https://jsonplaceholder.typicode.com/todos/1 sahifasiga so'rov yuboring
 va JSON javobini chiqarib bering.
 """
+r=requests.get('https://jsonplaceholder.typicode.com/todos/1')
+data=r.json()
+print(data)
