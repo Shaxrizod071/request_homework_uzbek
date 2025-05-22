8832562f08f3ddf6e830fbb368f17b1395b0bfc7
Shaxrizod071 <ahtamkulovshaxrizod@gmail.com>
2025-05-22 17:49:13 +0500

    dgj

--- topshiriq_8.py
+++ topshiriq_8.py
@@ -10,3 +10,10 @@ quyidagi ma'lumotlarni POST qilib yuboring:
 }
 Va javobdagi JSONni ko'rsating.
 """
+dct={
+    "title": "Mening sarlavham",
+    "body": "Bu mening birinchi postim.",
+    "userId": 1
+}
+r=requests.post('https://jsonplaceholder.typicode.com/posts', json=dct)
+print(list(r))
